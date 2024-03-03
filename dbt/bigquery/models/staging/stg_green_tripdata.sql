88bcc6083c8fcef8b328b3e52455280001d5653f
gregorywmorris <gwm1980@gmail.com>
2024-03-03 14:58:54 -0500

    dbt seed issue - ongoing

--- dbt/bigquery/models/staging/stg_green_tripdata.sql
+++ dbt/bigquery/models/staging/stg_green_tripdata.sql
@@ -3,12 +3,12 @@
 ) }}
 
 
-with yellow_cab_data as (
+with green_taxi_trips as (
     select
-        row_number() over(partition by VendorID, tpep_pickup_datetime) as row_num,
-        yt.*
+        row_number() over(partition by VendorID, lpep_pickup_datetime) as row_num,
+        gt.*
     from
-        {{ source('ny_taxi', 'yellow_cab_data') }} yt
+        {{ source('ny_taxi', 'green_cab_data') }} gt
     where
         VendorID is not null        
 )
@@ -16,35 +16,35 @@ with yellow_cab_data as (
 select
     -- identifiers
     {{ dbt_utils.generate_surrogate_key([
-        'VendorID', 
-        'tpep_pickup_datetime'
+        'VendorID',
+        'lpep_pickup_datetime'
     ]) }}                                as trip_id,
     VendorID                             as vendor_id,
     RatecodeID                           as ratecode_id,
     PULocationID                         as pickup_location_id,
     DOLocationID                         as dropoff_location_id,
     -- pickup and dropoff timestamps
-    tpep_pickup_datetime                 as pickup_datetime,
-    tpep_dropoff_datetime                as dropoff_datetime,
+    lpep_pickup_datetime                 as pickup_datetime,
+    lpep_dropoff_datetime                as dropoff_datetime,
     -- trip info
     store_and_fwd_flag                   as store_and_fwd_flag,
     passenger_count                      as passenger_count,
     trip_distance                        as trip_distance,
-    1                                    as trip_type, -- yellow cabs are always street-hail
+    trip_type                            as trip_type,
     -- payment info
     fare_amount                          as fare_amount,
     extra                                as extra,
     mta_tax                              as mta_tax,
     tip_amount                           as tip_amount,
     tolls_amount                         as tolls_amount,
-    0                                    as ehail_fee, -- it does not apply for yellow cabs
+    ehail_fee                            as ehail_fee,
     improvement_surcharge                as improvement_surcharge,
     congestion_surcharge                 as congestion_surcharge,
     total_amount                         as total_amount,
     payment_type                         as payment_type,
     {{ payment_desc_of('payment_type')}} as payment_type_desc
 from 
-    yellow_cab_data
+    green_taxi_trips
 where
     row_num = 1
 
